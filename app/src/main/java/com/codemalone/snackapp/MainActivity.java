85767b00fa18aa4f7c8521255b9d8ebbf183920e
Jared Malone <jaredmm@uw.edu>
2019-05-02 12:08:19 -0700

    main layout with recycler view

--- app/src/main/java/com/codemalone/snackapp/MainActivity.java
+++ app/src/main/java/com/codemalone/snackapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.codemalone.snackapp;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.codemalone.snackapp.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements MenuItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_content_menu, new MenuItemFragment())
+                    .commit();
+        }
     }
 
     @Override
@@ -48,5 +59,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
